470d7bf9dcac7d55ae3743c65af9f602240e3873
David Chauvière <david@chauviere.net>
2023-11-01 17:41:57 +0000

    init cover

---
+++ cover.go
@@ -0,0 +1,18 @@
+package cover
+
+type cover struct {
+	deviceId string
+	endpointId string
+	id string
+	name string
+	currentPosition int
+	uniqueId string
+	setPositionTopic string
+	positionTopic string
+	jsonAttributesTopic string
+	payloadOpen string
+	payloadClose string
+	payloadStop string
+	
+	
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dchauviere/go-tydom
+
+go 1.21.3
